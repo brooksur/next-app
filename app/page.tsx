8ff63849952fc46dace4374c91d53fd6b865f754
Brooks Benson <brooks@pdrutah.com>
2023-10-14 18:31:07 -0600

    wrap up

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import Link from 'next/link'
 import ProductCard from './components/ProductCard/ProductCard'
 import { getServerSession } from 'next-auth'
 import { authOptions } from './api/auth/[...nextauth]/route'
+import vercelImage from '@/public/next.svg'
 
 export default async function Home() {
   const session = await getServerSession(authOptions)
@@ -12,6 +13,8 @@ export default async function Home() {
       <h1>Hello {session && <span>{session.user!.name}</span>}</h1>
       <Link href="/users">Users</Link>
       <ProductCard />
+      <Image src={vercelImage} alt="Vercel" />
+      <Image src="https://bit.ly/react-cover" alt="React Logo" fill />
     </main>
   )
 }
--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        hostname: 'bit.ly',
+        protocol: 'https'
+      }
+    ]
+  }
+}
 
 module.exports = nextConfig
